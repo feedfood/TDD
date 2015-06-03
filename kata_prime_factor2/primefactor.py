023f0415b4dd814f692990254c559b8f1c06287c
Wei Xiao <wei.xiao@advantest.com>
2015-06-03 09:59:34 +0800

    primefactor.py

---
+++ kata_prime_factor2/primefactor.py
@@ -0,0 +1,11 @@
+import unittest
+
+class myTestSuite(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
