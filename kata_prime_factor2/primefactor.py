74f5615edbdaf94c49ba6f956c52fc01720e4519
Wei Xiao <wei.xiao@advantest.com>
2015-06-03 10:37:49 +0800

    pass 3, get [3]

--- kata_prime_factor2/primefactor.py
+++ kata_prime_factor2/primefactor.py
@@ -8,6 +8,8 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def prime(self, number):
+	if(number==3):
+	    return [3]
 	if(number==2):
 	    return [2]
 	return [] #return empty if number <= 1
@@ -17,6 +19,9 @@ class myTestSuite(unittest.TestCase):
 
     def test_2_return_2(self):
         self.assertEqual(self.prime(2), [2])
+
+    def test_3_return_3(self):
+        self.assertEqual(self.prime(3), [3])
 
 if __name__ == "__main__":
     unittest.main()
