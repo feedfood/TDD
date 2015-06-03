dfa6cd2ca54b6a830d7b5113941aa938b209e42b
Wei Xiao <wei.xiao@advantest.com>
2015-06-03 10:28:35 +0800

    pass 1, get empty

--- kata_prime_factor2/primefactor.py
+++ kata_prime_factor2/primefactor.py
@@ -7,5 +7,11 @@ class myTestSuite(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def prime(self, number):
+	return [] 
+
+    def test_1_return_empty(self):
+        self.assertEqual(self.prime(1), [])
+
 if __name__ == "__main__":
     unittest.main()
