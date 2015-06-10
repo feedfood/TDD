b23c80117d452ce1c674af8c7bf4a7b5150e1114
feedfood <codingboy@gmail.com>
2015-06-10 09:00:13 +0800

    pass 3 get fizz

--- kata_FizzBuzz/fizzbuzz.py
+++ kata_FizzBuzz/fizzbuzz.py
@@ -8,10 +8,15 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def speak(self, number):
+	if(number%3==0):
+	    return "fizz"
         return str(number)
 
-    def test_1_return_100(self):
+    def test_1_return_1(self):
         self.assertEqual(self.speak(1), "1")
+
+    def test_3_return_fizz(self):
+        self.assertEqual(self.speak(3), "fizz")
 
 if __name__ == "__main__":
     unittest.main()
