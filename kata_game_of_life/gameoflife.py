c01625ccb296eff15fd8e7c613fabf630f278780
feedfood <codingboy@gmail.com>
2015-06-12 13:01:45 +0800

    1x1 (0,0) return 0

--- kata_game_of_life/gameoflife.py
+++ kata_game_of_life/gameoflife.py
@@ -84,5 +84,10 @@ class myTestSuite(unittest.TestCase):
 	center=(1,1)
         self.assertEqual(self.getNeighbors(matrix,center), 5)
 
+    def test_1x1_return_0(self):
+	matrix=[[1]]
+	center=(0,0)
+        self.assertEqual(self.getNeighbors(matrix,center), 0)
+
 if __name__ == "__main__":
     unittest.main()
