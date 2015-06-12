9dcc275b726104d14aab09c56dd04a039c957f47
feedfood <codingboy@gmail.com>
2015-06-12 09:45:23 +0800

    333 return 8

--- kata_game_of_life/gameoflife.py
+++ kata_game_of_life/gameoflife.py
@@ -7,11 +7,18 @@ class myTestSuite(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def play(self, number):
-        return None 
+    def getNeighbors(self, matrix, center):
+	count=0
+	for x,row in enumerate(matrix):
+	    for y,cell in enumerate(row):
+		if(cell==1 and not(x==center[0] and y==center[1])):
+		    count+=1    
+        return count 
 
-    def test_1_return_1(self):
-        self.assertEqual(self.speak(1), "1")
+    def test_3x3x3_return_8(self):
+	matrix=((1,1,1),(1,1,1),(1,1,1))
+	center=(1,1)
+        self.assertEqual(self.getNeighbors(matrix,center), 8)
 
 if __name__ == "__main__":
     unittest.main()
