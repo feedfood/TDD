9279e00cd820d5aa408caa940dcf3e15a5fca505
feedfood <codingboy@gmail.com>
2015-06-26 13:28:09 +0800

    long word do wrap

--- kata_word_wrap2/wordwrap.py
+++ kata_word_wrap2/wordwrap.py
@@ -10,7 +10,11 @@ class myTestSuite(unittest.TestCase):
     def wrap(self, s, length):
         if( s == None):
             return "";
-        return s; 
+            
+        if(len(s)<=length):
+            return s;
+            
+        return s[:length] + "\n" + s[length:] 
 
     def testWrap_Null_Returns_Empty(self):
         self.assertEqual(self.wrap(None,10),"")
@@ -20,6 +24,9 @@ class myTestSuite(unittest.TestCase):
 
     def testWrap_OneShortWord_Not_Wrap(self):
         self.assertEqual(self.wrap("word",10),"word")
+        
+    def testWrap_OneLongWord_Wrap(self):
+        self.assertEqual(self.wrap("longword",5),"longw\nord")        
 
 if __name__ == "__main__":
     unittest.main()
