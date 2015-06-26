3d6d8a836fb2ebd8dcd3f6e6cc65a1ed420bb711
feedfood <codingboy@gmail.com>
2015-06-26 13:20:17 +0800

    One short word does not wrap

--- kata_word_wrap2/wordwrap.py
+++ kata_word_wrap2/wordwrap.py
@@ -8,13 +8,18 @@ class myTestSuite(unittest.TestCase):
         pass
 
     def wrap(self, s, length):
-        return ""
+        if( s == None):
+            return "";
+        return s; 
 
     def testWrap_Null_Returns_Empty(self):
         self.assertEqual(self.wrap(None,10),"")
 
     def testWrap_Empty_Returns_Empty(self):
         self.assertEqual(self.wrap("",10),"")
+
+    def testWrap_OneShortWord_Not_Wrap(self):
+        self.assertEqual(self.wrap("word",10),"word")
 
 if __name__ == "__main__":
     unittest.main()
